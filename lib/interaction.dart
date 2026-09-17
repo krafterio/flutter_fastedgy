26382ab64abda81ec947969da55f5cc9c5315173
François Pluchino <francois@krafter.io>
2026-09-17 14:44:50 +0200

    feat(ui): add responsive and interaction entry libraries
    - let an application import the breakpoints and the hover pointer without a show clause, since ui.dart also exports names that clash with material

---
+++ lib/interaction.dart
@@ -0,0 +1,8 @@
+/*
+ * Copyright Krafter SAS <developer@krafter.io>
+ * MIT License (see LICENSE file).
+ */
+
+library;
+
+export 'src/ui/interaction.dart';
---
+++ lib/responsive.dart
@@ -0,0 +1,9 @@
+/*
+ * Copyright Krafter SAS <developer@krafter.io>
+ * MIT License (see LICENSE file).
+ */
+
+library;
+
+export 'src/ui/responsive.dart';
+export 'src/ui/theme/breakpoints.dart';
